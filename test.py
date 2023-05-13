842c76b6ce0806811be97dc9c29fa8b6d2735aec
AayushBaniya2006 <thisisaayushbaniya@gmail.com>
2023-05-13 00:39:15 -0500

    TensorFlow Updates

--- test.py
+++ test.py
@@ -1,32 +1,46 @@
-from sklearn import datasets
+import pandas as pd
+
+# Load the CSV file
+df = pd.read_csv('export/_annotations.csv')
+
+# Preprocess the dataset
+df = df[['filename', 'class', 'xmin', 'ymin', 'xmax', 'ymax']]
+df['class'] = df['class'].astype('category').cat.codes
+df['filename'] = df['filename'].apply(lambda x: 'path/to/images/' + x)
+df['width'] = 512
+df['height'] = 512
+
+# Split the dataset
 from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
-from sklearn.metrics import classification_report
 
-dataset = datasets.load_files("path/to/car/images", shuffle=True)
+X = df.drop('class', axis=1)
+y = df['class']
 
-# Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(dataset.data, dataset.target, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Extract features from the images using HOG
-from skimage.feature import hog
-from skimage import color
-import numpy as np
 
-def extract_features(X):
-    features = []
-    for img in X:
-        gray_img = color.rgb2gray(img)
-        features.append(hog(gray_img, block_norm='L2', pixels_per_cell=(32, 32), cells_per_block=(1, 1)))
-    return np.array(features)
+roses = list(data_dir.glob('export/_annotations.csv'))
 
-X_train_features = extract_features(X_train)
-X_test_features = extract_features(X_test)
+batch_size = 100
+img_height = 512
+img_width = 512
 
-# Train an SVM classifier on the extracted features
-clf = SVC(kernel='linear')
-clf.fit(X_train_features, y_train)
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
 
-# Evaluate the model on the testing set
-y_pred = clf.predict(X_test_features)
-print(classification_report(y_test, y_pred))
+val_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+class_names = train_ds.class_names
+print(class_names)
+
