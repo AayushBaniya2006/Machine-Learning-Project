83f3539cebdd3d065e195222b515944adc1472a7
Johan_Park <johan.park97@gmail.com>
2023-05-13 21:59:39 -0500

    idk

--- tutorial/Categorical_cross-entropy.py
+++ tutorial/Categorical_cross-entropy.py
@@ -8,5 +8,6 @@ target_output = [1, 0, 0]
 loss = -(math.log(softmax_output[0])*target_output[0] +
          math.log(softmax_output[1])*target_output[1] +
          math.log(softmax_output[2])*target_output[2])
+#All that can be simplified to loss = -math.log(softmax_output[0]), because of maths stuff
 
 print(loss)
